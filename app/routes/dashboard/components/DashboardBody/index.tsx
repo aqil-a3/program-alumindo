62e0e27ae8c9187e2ee7427490edb7f60121ce5d
aqil <aqilalumindo@gmail.com>
2024-08-22 11:57:03 +0700

    update

--- app/routes/dashboard/components/DashboardBody/index.tsx
+++ app/routes/dashboard/components/DashboardBody/index.tsx
@@ -4,6 +4,11 @@ export default function DashboardBody(){
 return(
     <div className="py-8">
         <Shortcut />
+        <div className="grid grid-cols-3">
+            <div>Live Chat</div>
+            <div>Notifikasi</div>
+            <div>Aktifitas Terakhir</div>
+        </div>
     </div>
 )
 }
